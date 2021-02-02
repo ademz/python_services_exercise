f8249f6fc34d622032fa294ba126fd9690eb5d80
adem oztas <adem@oztas.net>
2021-02-02 00:11:58 -0500

    micro service implementation

--- src/api_service.py
+++ src/api_service.py
@@ -1,27 +1,57 @@
-import time
-import uuid
 import boto3
+import json
+import uuid
+
 from decouple import config
+from flask_api import FlaskAPI
+from flask_restful import reqparse, Api, Resource
+
 
 LOCALSTACK_SQS_ENDPOINT_URL = config("LOCALSTACK_SQS_ENDPOINT_URL")
 BOOKING_QUEUE_URL = config("BOOKING_QUEUE_URL")
 
 sqs = boto3.client("sqs", endpoint_url=LOCALSTACK_SQS_ENDPOINT_URL)
 
-while (True):
-    time.sleep(3)
-    try:
-        response = sqs.send_message(
-            QueueUrl=BOOKING_QUEUE_URL,
-            MessageBody="body",
-            MessageDeduplicationId=str(uuid.uuid4()),
-            MessageGroupId="messages",
-            MessageAttributes={
-                "contentType": {
-                    "StringValue": "application/json", "DataType": "String"}
-            }
-        )
-        print("posted")
-    except:
-        print("SQS send message failure")
-        time.sleep(3)
+app = FlaskAPI(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('time')
+
+
+class Reservation(Resource):
+
+    def post(self):
+        args = parser.parse_args()
+        message_id = str(uuid.uuid4())
+
+        values = {}
+        # @TODO implement form validation!
+        values['name'] = args.get('name', '')
+        values['time'] = args.get('time', '')
+        values['message_id'] = message_id
+
+        try:
+            sqs.send_message(
+                QueueUrl=BOOKING_QUEUE_URL,
+                MessageBody=json.dumps(values),
+                MessageDeduplicationId=message_id,
+                MessageGroupId="messages",
+                MessageAttributes={
+                    "contentType": {
+                        "StringValue": "application/json",
+                        "DataType": "String"
+                    }
+                }
+            )
+        except Exception as e:
+            print('message failed: ', e)
+
+        return {'message': 'Your booking was added to the queue.'}
+
+
+api.add_resource(Reservation, '/reservation')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
--- src/booking_service.py
+++ src/booking_service.py
@@ -1,5 +1,4 @@
-import time
-import uuid
+import asyncio
 import boto3
 from decouple import config
 
@@ -8,14 +7,40 @@ BOOKING_QUEUE_URL = config("BOOKING_QUEUE_URL")
 
 sqs = boto3.client("sqs", endpoint_url=LOCALSTACK_SQS_ENDPOINT_URL)
 
-while (True):
-    try:
-        messages = sqs.receive_message(QueueUrl=BOOKING_QUEUE_URL,
-                                       AttributeNames=['All'], MaxNumberOfMessages=10, WaitTimeSeconds=2, VisibilityTimeout=30)
 
-        messages = messages.get("Messages", [])
+def create_booking(booking):
+    # @TODO implement db
+    print(booking)
 
-        print("Messages", messages)
-    except:
-        print("SQS receive message failure")
-        time.sleep(5)
+
+async def listen():
+    while True:
+        try:
+            messages = sqs.receive_message(
+                QueueUrl=BOOKING_QUEUE_URL, AttributeNames=['All'],
+                MaxNumberOfMessages=10, WaitTimeSeconds=2, VisibilityTimeout=30
+            )
+
+            messages = messages.get("Messages", [])
+            for message in messages:
+                handler_id = message['ReceiptHandle']
+                create_booking(message['Body'])
+                sqs.delete_message(
+                    QueueUrl=BOOKING_QUEUE_URL,
+                    ReceiptHandle=handler_id
+                )
+
+            # print("Messages", messages)
+        except Exception as e:
+            print("SQS receive message failure: ", e)
+
+
+loop = asyncio.get_event_loop()
+
+try:
+    asyncio.ensure_future(listen())
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    loop.close()
